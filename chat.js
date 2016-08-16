c6c6fad51d5edddc78989aa9bc4551129cc32602
EugeneSolovyev <eugenesolovyev92@gmail.com>
2016-08-16 18:24:14 +0300

    chat prototype

---
+++ chat.js
@@ -0,0 +1,26 @@
+/**
+ * Created by eugene on 16.8.16.
+ */
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var port = 3000;
+logger.debug('Script has been started...');
+server.listen(port);
+app.use(express.static(__dirname + '/public'));
+io.on('connection', function (socket) {
+    var name = 'User' + (socket.id).toString().substr(1,4);
+    socket.broadcast.emit('newUser', name);
+    logger.info(name + ' connected to chat!');
+    socket.emit('userName', name);
+
+    socket.on('message', function(msg){
+        logger.warn('-----------');
+        logger.warn('User: ' + name + ' | Message: ' + msg);
+        logger.warn('====> Sending message to other chaters...');
+        io.sockets.emit('messageToClients', msg, name);
+    });
+});
